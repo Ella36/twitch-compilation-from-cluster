41f8680007ced3ce28b301289cdec1f23209c7dc
Sogla <soglahash@gmail.com>
2022-06-11 00:29:14 +0200

    Update clip selection

--- clip_selector.py
+++ clip_selector.py
@@ -3,52 +3,61 @@
 # Keep in mind view count, duration to till 10mins and avoid duplicates
 import pandas as pd
 from pathlib import Path
-import re
 import random
+from datetime import datetime
 
 URLS = Path('./urls')
 
-# Select creators
-creators = ['gladd', 'summit1g', 'shroud', 'smoke', 'cdnthe3rd']
+# Select cluster
+from cluster.cluster import CLUSTERS
+cluster = 'cluster1'
+creators = CLUSTERS.by_name(cluster).names
 
-def parse_int(x):
-    # 18.2K views
-    # 28 views
-    if 'k' in x.lower():
-        return int(float(re.match(r'\d+\.*\d*', x)[0])*1000)
-    else:
-        return int(re.match(r'\d+', x)[0])
+import pandas as pd
+from db.mydb import Mydb
+MYDB = Mydb()
 
+# Read sqlite query results into a pandas DataFrame
+creators_str = '('+','.join([f"'{c}'" for c in creators])+')'
+df = pd.read_sql_query(f"SELECT * FROM clips WHERE creator IN {creators_str}", MYDB.con)
 
-clips_info = []
-# Read data
-for creator in creators:
-    clip_info_file = URLS / Path(creator+'.txt')
-    clip_info = pd.read_csv(
-        clip_info_file,
-        delimiter=',',
-        names=['url','duration','views','interval'],
-        )
-    # Convert to usable
-    clip_info['views'] = clip_info['views'].apply(parse_int)
-    clip_info['duration'] = clip_info['duration'].apply(
-        lambda x: int(re.findall(r'\d+', x)[0]) * 60 + int(re.findall(r'\d+', x)[1]))
-    clip_info['creator'] = creator
-    clips_info.append(clip_info)
+# Verify that result of SQL query is stored in the dataframe
+print(df.head())
 
-# Concatenate
-df_clip_info = pd.concat(clips_info, axis=0)
+# Close connection
+MYDB.con.close()
 
-# Select URLs
-# Rotate through each creator, select highest viewed clip
-df_clip_info.sort_values(by=['views','duration'])
-df = df_clip_info
-df['views'].describe()
-urls = []
-views = []
+# Discard invalid clips
+df = df[df['published']==0]
+
+import datetime
+from dateutil.relativedelta import relativedelta
+def get_past_date(days=7):
+    TODAY = datetime.date.today()
+    date = TODAY - relativedelta(days=days)
+    return str(date.isoformat())
+
+days = 7
+df = df[df['time'] >= get_past_date(days=days)]
+
+# Select best clips
 max_duration = 610
 duration = 0
-for creator in creators*100:
+top_urls = []
+
+# Select 2 highest views clips
+count = 0
+n_high_view_first = 2
+df.sort_values(by=['views','duration'])
+for idx, row in df.iterrows():
+    top_urls.append(row.url)
+    duration += row.duration
+    if len(top_urls) >= n_high_view_first:
+        break
+
+# Pick from creators
+urls = []
+for creator in creators*10:
     for idx, row in df.iterrows():
         if row.creator != creator:
             continue
@@ -58,35 +67,9 @@ for creator in creators*100:
             continue
         # Add
         urls.append(row.url)
-        views.append(row.views)
         duration += row.duration
         break
 
-print(duration)
-print(urls)
-
-# Move 3 clips with highest views to the top
-url_sorted = []
-
-most_views = max(views)
-idx = views.index(most_views)
-views.remove(most_views)
-url = urls.pop(idx)
-url_sorted.append(url)
-
-most_views = max(views)
-idx = views.index(most_views)
-views.remove(most_views)
-url = urls.pop(idx)
-url_sorted.append(url)
-
-most_views = max(views)
-idx = views.index(most_views)
-views.remove(most_views)
-url = urls.pop(idx)
-url_sorted.append(url)
-
 random.shuffle(urls)
-url_sorted += urls
 
-Path('urls.txt').write_text('\n'.join(url_sorted))
+Path('urls.txt').write_text('\n'.join(top_urls+urls))
