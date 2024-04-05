2b12307c61b8cc853602051861f8e603960b9828
Jelle Meeus <jellemeeus@hotmail.com>
2024-04-05 13:49:11 +0200

    autoformat

--- src/create_schedule.py
+++ src/create_schedule.py
@@ -18,13 +18,14 @@ class Release:
     name: str
     interval: int
 
+
 class Day:
     def __init__(self, releases):
         self.releases = releases
 
     def add(self, release):
         self.releases.append(release)
-    
+
     @property
     def cost(self):
         return sum([r.interval for r in self.releases])
@@ -32,31 +33,34 @@ class Day:
     def __str__(self):
         return f'{self.cost}-' + ', '.join([str(r) for r in self.releases])
 
+
 class ReleaseSolver:
     def __init__(self, initial, target_interval_per_day):
-        self.initial = initial # Array to manipulate
+        self.initial = initial  # Array to manipulate
         self.n_iterations = 1000
         self.target_interval_per_day = target_interval_per_day
 
     def cost(self, candidate):
-        avg_interval_day_cost = sum([abs(self.target_interval_per_day-d.cost) for d in candidate])
+        avg_interval_day_cost = sum(
+            [abs(self.target_interval_per_day-d.cost) for d in candidate])
         spread_cost = 0
         for day_idx, d in enumerate(candidate):
             for r in d.releases:
                 release_name = r.name
                 # Search for closest release_name
                 for day2_idx, d2 in enumerate(candidate):
-                    n = [r2.name.split('_')[0] for r2 in d2.releases].count(release_name.split('_')[0])
+                    n = [r2.name.split('_')[0] for r2 in d2.releases].count(
+                        release_name.split('_')[0])
                     if day2_idx == day_idx:
                         n -= 1
                     spread_cost += abs(n*(r.interval - abs(day2_idx-day_idx)))
-        return avg_interval_day_cost * 2 + spread_cost / 10 # Guessing with /3
+        return avg_interval_day_cost * 2 + spread_cost / 10  # Guessing with /3
 
     def manipulate(self, current, step_size):
         return current
 
     def shuffle(self, current):
-        releases = [] 
+        releases = []
         for d in current:
             releases += d.releases
         releases_copy = releases[:]
@@ -86,7 +90,7 @@ class ReleaseSolver:
                 # Store new best
                 best, best_eval = candidate, candidate_eval
             # difference between candidate and current point
-            diff = candidate_eval - curr_eval 
+            diff = candidate_eval - curr_eval
             # calculate temperature for current epoch
             t = temp / float(i+1)
             # calculate metropolis acceptance cirterion
@@ -96,6 +100,7 @@ class ReleaseSolver:
                 # store new current point
                 curr, curr_eval = candidate, candidate_eval
         return [best, best_eval]
+
 
 def is_prompt_confirm(step: str):
     questions = [
@@ -109,18 +114,22 @@ def is_prompt_confirm(step: str):
     answers = prompt(questions)
     return answers['confirm']
 
+
 def argparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--today", action="store_true", help="ignores date and starts from today")
-    parser.add_argument('-d', '--date', default='2022-07-21', help='start date in format: YYYY-MM-DD')
+    parser.add_argument("-t", "--today", action="store_true",
+                        help="ignores date and starts from today")
+    parser.add_argument('-d', '--date', default='2022-07-21',
+                        help='start date in format: YYYY-MM-DD')
     return parser.parse_args()
+
 
 def print_days_since_release():
     db = Mydb()
     df = db.read_compilations_df_from_db()
     latest_release_by_category = {}
     for _, row in df.iterrows():
-        #print(row.id, row.time, row.project)
+        # print(row.id, row.time, row.project)
         published_at = datetime.datetime.strptime(row.time, "%Y-%m-%d")
         now = datetime.datetime.utcnow()
         days_since = (now - published_at).days
@@ -132,6 +141,7 @@ def print_days_since_release():
     for category, days_since_release in latest_release_by_category.items():
         print(f"{category:20} {days_since_release:3}")
     db.close()
+
 
 if __name__ == '__main__':
     args = argparser()
@@ -148,7 +158,6 @@ if __name__ == '__main__':
         releases += [Release(p.name, p.interval)] * p.n_per_month
     n = len(releases)
     print(f'Videos to release:\n\tn {n}')
-
 
     # Create schedule
     if args.today:
@@ -174,29 +183,30 @@ if __name__ == '__main__':
             i = i % 30
             r = releases_copy.pop()
             initial_release_by_day[i].add(r)
-        release_solver = ReleaseSolver(initial_release_by_day, target_interval_per_day)
+        release_solver = ReleaseSolver(
+            initial_release_by_day, target_interval_per_day)
         releases_by_day, cost = release_solver.simulated_annealing()
 
         n = sum([len(d.releases) for d in releases_by_day])
         print(f'Videos released:\n\tn {n}')
 
         for i, day in enumerate(releases_by_day):
-            time=start_time + timedelta(days=i)
+            time = start_time + timedelta(days=i)
             day_format = ', '.join([r.name for r in day.releases])
             print(f'{i:02d} {time.date()} {day.cost} {day_format}')
         is_generated_schedule = True
 
     if is_generated_schedule:
         # Write solution somewhere
-        schedule = {} # 'date': isoformat, 'projects': list str
+        schedule = {}  # 'date': isoformat, 'projects': list str
         for i, day in enumerate(releases_by_day):
-            time=start_time + timedelta(days=i)
+            time = start_time + timedelta(days=i)
             project_names = [r.name for r in day.releases]
             date = str(time.date())
             schedule[date] = project_names
 
-    file = Path('./schedule.text') 
-    if is_generated_schedule: 
+    file = Path('./schedule.text')
+    if is_generated_schedule:
         text = ''
         for date, project_names in schedule.items():
             text += '-\n'
@@ -234,17 +244,6 @@ if __name__ == '__main__':
         print(out)
         # Write to file
         if is_prompt_confirm('Write commands to cmds.txt'):
-            file = Path('./cmds.txt') 
+            file = Path('./cmds.txt')
             with file.open('w') as f:
                 f.write(out)
-
-    
-    
-
-
-
-
-
-
-
-
